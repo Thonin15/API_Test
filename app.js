32d77a58281063d874f190854f3a41e997932a12
Thonin15 <164446643+Thonin15@users.noreply.github.com>
2024-04-11 22:45:12 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+const uri = "mongodb+srv://kitku:<password>@cluster0.imwxnou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect('mongodb+srv://kitku:admin12345@Cluster0.imwxnou.mongodb.net/locationDB', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB', err));
+
 const PORT = process.env.PORT || 3000;
+const Location = require('./models/Location');
 
 app.use(express.json());
 
@@ -8,18 +17,14 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// Define route handler for POST requests to /api/locations
-app.post('/api/locations', (req, res) => {
-  const locationData = req.body;
-  console.log('Received location data:', locationData);
-  
-  // Handle location data (e.g., store in a database)
-  
-  res.status(200).json({ message: 'Location data received successfully' });
-});
-
-// Define route handler for other endpoints
-// This handler will respond with a 404 Not Found for any other requests
-app.use((req, res) => {
-  res.status(404).send('Not Founds');
+app.post('/api/locations', async (req, res) => {
+  try {
+    const { latitude, longitude } = req.body;
+    const location = new Location({ latitude, longitude });
+    await location.save();
+    res.status(200).json({ message: 'Location data saved successfully' });
+  } catch (error) {
+    console.error('Failed to save location data:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
