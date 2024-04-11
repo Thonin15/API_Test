0bfc4a321ff85b310360eb581e545e6adea67848
Thonin15 <164446643+Thonin15@users.noreply.github.com>
2024-04-12 00:38:09 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Location = require('./models/Location');
 
 // Connect to MongoDB
-const uri = "mongodb+srv://kitku:<password>@cluster0.imwxnou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect('mongodb+srv://kitku:admin12345@Cluster0.imwxnou.mongodb.net/locationDB', { useNewUrlParser: true, useUnifiedTopology: true })
+const uri = "mongodb+srv://kitku:admin12345@cluster0.imwxnou.mongodb.net/locationDB";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB', err));
 
 const PORT = process.env.PORT || 3000;
-const Location = require('./models/Location');
 
 app.use(express.json());
 
@@ -17,6 +17,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+// POST endpoint to save location data
 app.post('/api/locations', async (req, res) => {
   try {
     const { latitude, longitude } = req.body;
@@ -25,6 +26,18 @@ app.post('/api/locations', async (req, res) => {
     res.status(200).json({ message: 'Location data saved successfully' });
   } catch (error) {
     console.error('Failed to save location data:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// GET endpoint to fetch location data
+app.get('/api/locations', async (req, res) => {
+  try {
+    // Retrieve all location documents from the database
+    const locations = await Location.find({}, 'latitude longitude');
+    res.json(locations);
+  } catch (error) {
+    console.error('Failed to fetch location data:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
